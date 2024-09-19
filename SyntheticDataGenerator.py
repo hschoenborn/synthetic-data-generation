050bc3ef19aac4430221168bdb3775ac0d56015c
hschoenborn <150366213+hschoenborn@users.noreply.github.com>
2024-09-19 08:57:12 +0200

    PAR Hannah (#3)
    * feature: implement par
    
    - include Measurement ID
    - create general metadata
    - include identifiers (time, interval, id) in generation
    - plot column distributions
    
    * style: create folder structure
    
    - dataset: the real data to upload
    - output: save generated datasets here
    - api_request_stablenet: script to fetch stablenet datasets

--- SyntheticDataGenerator.py
+++ SyntheticDataGenerator.py
@@ -1,16 +1,22 @@
-import pandas as pd
+# Standard library imports
 from datetime import datetime
-from sdv.metadata import SingleTableMetadata
-from sdv.single_table import TVAESynthesizer, CTGANSynthesizer
-import pickle
 from pathlib import Path
+import pickle
+
+# Third-party library imports
+import matplotlib.pyplot as plt
+import pandas as pd
 import plotly.graph_objects as go
+import seaborn as sns
 from typing import List, Optional
 from sdv.evaluation.single_table import run_diagnostic, evaluate_quality
+from sdv.metadata import SingleTableMetadata
+from sdv.single_table import TVAESynthesizer, CTGANSynthesizer
+from sdv.sequential import PARSynthesizer
 
 class SyntheticDataGenerator:
     def __init__(self, metadata_dict: dict, model_type: str, epochs: int = 300, batch_size: int = 16, cuda: bool = True,
-                 enforce_rounding: bool = True, enforce_min_max_values: bool = True,
+                 enforce_rounding: bool = True, enforce_min_max_values: bool = False,
                  selected_columns: Optional[List[str]] = None):
         """
         Initialize the Synthetic Data Generator with model configuration and metadata.
@@ -34,6 +40,8 @@ class SyntheticDataGenerator:
 
         filtered_metadata = {
             "METADATA_SPEC_VERSION": metadata_dict["METADATA_SPEC_VERSION"],
+            "sequence_key": "Measurement ID",
+            "sequence_index": "Time",
             "columns": {k: v for k, v in metadata_dict["columns"].items() if k in selected_columns}
         }
         return SingleTableMetadata().load_from_dict(filtered_metadata)
@@ -73,6 +81,14 @@ class SyntheticDataGenerator:
                 epochs=self.epochs,
                 cuda=self.cuda,
             )
+        elif self.model_type == "par":
+            self.model = PARSynthesizer(
+                metadata=self.metadata,
+                enforce_rounding=self.enforce_rounding,
+                enforce_min_max_values=self.enforce_min_max_values,
+                epochs=self.epochs,
+                cuda=self.cuda,
+            )
         else:
             raise ValueError("Unsupported model type")
 
@@ -83,7 +99,8 @@ class SyntheticDataGenerator:
         if self.model is None:
             self.initialize_model()
         self.model.fit(real_data_df)
-        self.losses_df = self.model.get_loss_values()
+        if self.model_type != 'par':
+            self.losses_df = self.model.get_loss_values()
 
     def save_model(self, model_folder: Path, model_number: int = 1):
         """
@@ -108,13 +125,24 @@ class SyntheticDataGenerator:
         with open(model_path, 'rb') as f:
             self.model = pickle.load(f)
 
-    def generate_synthetic_data(self, num_samples: int) -> pd.DataFrame:
+    def generate_synthetic_data_simple(self, num_samples: int) -> pd.DataFrame:
         """
-        Generate synthetic data using the trained model.
+        Generate synthetic data using the trained simple table model (CTGAN, TVAE).
         """
         if self.model is None:
             raise ValueError("Model is not trained or loaded.")
         synthetic_data = self.model.sample(num_samples)
+        return synthetic_data
+
+    def generate_synthetic_data_par(self, num_sequences: int) -> pd.DataFrame:
+        """
+        Generate synthetic data using the trained sequential table model (PAR).
+        """
+        if self.model is None:
+            raise ValueError("Model is not trained or loaded.")
+        synthetic_data = self.model.sample(num_sequences=num_sequences)
+        # PAR uses only one measurement ID (sequence=1) in our use case
+
         return synthetic_data
 
     def save_synthetic_data(self, synthetic_data: pd.DataFrame, output_file: str):
@@ -177,6 +205,49 @@ class SyntheticDataGenerator:
             metadata=self.metadata
         )
         return quality_report
+
+    def plot_column_distributions(self, real_data_df: pd.DataFrame, synthetic_data: pd.DataFrame):
+        """
+        Evaluate the column distributions of the generated synthetic data in comparison to the real data.
+        """
+        # Define the number of columns in the DataFrames
+        num_columns = len(real_data_df.columns)
+
+        # Create subplots: num_columns rows and 2 columns
+        fig, axes = plt.subplots(nrows=num_columns, ncols=2, figsize=(12, 3 * num_columns), constrained_layout=True)
+
+        # Iterate through columns and plot
+        for i, column in enumerate(real_data_df.columns):
+            # Plot real data distribution
+            sns.histplot(real_data_df[column], ax=axes[i, 0], kde=True, bins=30)
+            axes[i, 0].set_title(f'Real Data: {column}')
+
+            # Plot synthetic data distribution
+            sns.histplot(synthetic_data[column], ax=axes[i, 1], kde=True, bins=30)
+            axes[i, 1].set_title(f'Synthetic Data: {column}')
+
+            # Set the same x and y limits and ticks for both plots
+            x_min = min(real_data_df[column].min(), synthetic_data[column].min())
+            x_max = max(real_data_df[column].max(), synthetic_data[column].max())
+            y_min = 0
+            y_max = max(axes[i, 0].get_ylim()[1], axes[i, 1].get_ylim()[1])
+
+            axes[i, 0].set_xlim(x_min, x_max)
+            axes[i, 1].set_xlim(x_min, x_max)
+            axes[i, 0].set_ylim(y_min, y_max)
+            axes[i, 1].set_ylim(y_min, y_max)
+
+            # Set the same x and y ticks
+            x_ticks = sorted(set(axes[i, 0].get_xticks()).union(set(axes[i, 1].get_xticks())))
+            y_ticks = sorted(set(axes[i, 0].get_yticks()).union(set(axes[i, 1].get_yticks())))
+
+            axes[i, 0].set_xticks(x_ticks)
+            axes[i, 1].set_xticks(x_ticks)
+            axes[i, 0].set_yticks(y_ticks)
+            axes[i, 1].set_yticks(y_ticks)
+
+        # Show the plot
+        plt.show()
 
     def plot_time_series_comparison(self, real_data_df: pd.DataFrame, synthetic_data_df: pd.DataFrame,
                                     column: str) -> go.Figure:
--- streamlit_measurements.py
+++ streamlit_measurements.py
@@ -1,14 +1,15 @@
-import streamlit as st
-import pandas as pd
-import json
+# Standard library imports
 from datetime import datetime
-from sdmetrics.reports.single_table import QualityReport
-from sdv.metadata import SingleTableMetadata
+import json
+
+# Third-party library imports
+import pandas as pd
+import streamlit as st
 from SyntheticDataGenerator import SyntheticDataGenerator
 from database import SessionLocal, create_tables, SyntheticDataFile
-from sqlalchemy.orm import Session
+from sdmetrics.reports.single_table import QualityReport
+from sdv.metadata import SingleTableMetadata
 from value_triple_mapping import ValueTripleMapping
-import os
 
 # Initialize the database
 create_tables()
@@ -29,29 +30,40 @@ def preprocess_timestamps(df: pd.DataFrame, column: str = 'Time') -> pd.DataFram
 
 
 def filter_metadata(metadata_dict, selected_columns):
-    return {
+    st.info(f"Selected columns: {selected_columns}")
+    retval = {
         "METADATA_SPEC_VERSION": metadata_dict["METADATA_SPEC_VERSION"],
+        "sequence_key": metadata_dict["sequence_key"],
+        "sequence_index": metadata_dict["sequence_index"],
         "columns": {k: v for k, v in metadata_dict["columns"].items() if k in selected_columns}
     }
+    # st.info(retval)  # DEBUG
+    return retval
 
 
 def get_selected_columns(value_triples, mapping, slim_columns_only):
-    selected_columns_for_generation = []
-    selected_columns_for_display = ["Time", "Interval"]
+    selected_columns_for_generation = ["Measurement ID", "Time", "Interval"]
+    # selected_columns_for_display = ["Time", "Interval"]
 
     for triple in value_triples:
         if slim_columns_only:
             selected_columns_for_generation.append(mapping[triple][2])
-            selected_columns_for_display.append(mapping[triple][2])
+            # selected_columns_for_display.append(mapping[triple][2])
         else:
             selected_columns_for_generation.extend(mapping[triple])
-            selected_columns_for_display.extend(mapping[triple])
+            # selected_columns_for_display.extend(mapping[triple])
 
-    return selected_columns_for_generation, selected_columns_for_display
+    return selected_columns_for_generation  #, selected_columns_for_display
 
 
 def upload_and_process_file(uploaded_file, column='Time'):
     real_data_df = pd.read_csv(uploaded_file, delimiter=';')
+    st.info("You must provide the Measurement ID (sequence) of your dataset", icon='⚠️')
+    measurement_id = st.text_input("Measurement ID:")
+    real_data_df['Measurement ID'] = measurement_id
+    columns = ['Measurement ID'] + [col for col in real_data_df if col != 'Measurement ID']
+    real_data_df = real_data_df[columns]
+
     return preprocess_timestamps(real_data_df, column=column)
 
 
@@ -69,7 +81,7 @@ def generate_data_app():
     st.title("Synthetic Data Generator")
 
     # Add a header image
-    st.image("logos/uni_logo.jpg", width=200)
+    # st.image("logos/uni_logo.jpg", width=200)
 
     # File upload section
     st.subheader("Step 1: Upload Your Data")
@@ -93,17 +105,23 @@ def generate_data_app():
             list(value_triple_mapping.keys())  # Default selected
         )
 
-        non_generation_columns = ["Time", "Interval"]
-        selected_columns_for_generation, selected_columns_for_display = get_selected_columns(
+        # non_generation_columns = ["Time", "Interval"]
+        selected_columns_for_generation = get_selected_columns(
             value_triples, value_triple_mapping, slim_columns_only
         )
 
-        real_data_df = real_data_df[selected_columns_for_display]
+        # selected_columns_for_generation, selected_columns_for_display = get_selected_columns(
+        #     value_triples, value_triple_mapping, slim_columns_only
+        # )
+        #
+        # # TODO: drop measurement id column in time-series comparisons
+        #
+        real_data_df = real_data_df[selected_columns_for_generation]  # selected_columns_for_display
         metadata = SingleTableMetadata().load_from_dict(filter_metadata(metadata_dict, selected_columns_for_generation))
 
         # Model settings
         st.subheader("Step 3: Select Model and Training Settings")
-        model_type = st.selectbox("Select model type", ["tvae", "ctgan"])
+        model_type = st.selectbox("Select model type", ["par", "tvae", "ctgan"])
         epochs = st.number_input("Enter number of epochs", min_value=1, max_value=1000, value=300)
 
         # Model training
@@ -115,23 +133,31 @@ def generate_data_app():
                         selected_columns=selected_columns_for_generation
                     )
                     generator.train_model(real_data_df[selected_columns_for_generation])
-                    fig = generator.visualize_loss_values()
-                    st.plotly_chart(fig)
                     st.session_state['generator'] = generator
                     st.success("Model trained successfully.")
+                    if model_type != "par":
+                        fig = generator.visualize_loss_values()
+                        st.plotly_chart(fig)
                 except Exception as e:
                     st.error(f"Error during training: {e}")
+                    st.error(metadata.to_dict())  # TODO: Delete
 
         # Data generation
         if 'generator' in st.session_state:
             st.subheader("Step 4: Generate and Download Synthetic Data")
-            num_samples = len(real_data_df)
+
             if st.button("Generate Synthetic Data"):
                 with st.spinner('Generating synthetic data...'):
                     try:
-                        synthetic_data = st.session_state['generator'].generate_synthetic_data(num_samples)
-                        synthetic_data_with_time = pd.concat([real_data_df[non_generation_columns], synthetic_data],
-                                                             axis=1)
+                        if model_type != "par":
+                            num_samples = len(real_data_df)
+                            synthetic_data = st.session_state['generator'].generate_synthetic_data_simple(num_samples)
+                        else:
+                            num_sequences = int(real_data_df['Measurement ID'].nunique())  # 1 sequence per ID
+                            synthetic_data = st.session_state['generator'].generate_synthetic_data_par(num_sequences)
+
+                        synthetic_data_with_time = synthetic_data
+                        # pd.concat([real_data_df[non_generation_columns], synthetic_data], axis=1)
 
                         # Store synthetic data in session state for later use
                         st.session_state['synthetic_data'] = synthetic_data
@@ -146,6 +172,29 @@ def generate_data_app():
         # Save and evaluate options
         if 'synthetic_data_with_time' in st.session_state:
             st.subheader("Step 5: Save or Evaluate Synthetic Data")
+
+            if st.button("Save Synthetic Data to Database without ID column"):
+                with st.spinner('Saving synthetic data to database...'):
+                    try:
+                        generate_downloadable_csv(
+                            st.session_state['generator'].postprocess_timestamps(
+                                st.session_state['synthetic_data_with_time'].copy()
+                            )
+                        )
+                        synthetic_data = st.session_state['synthetic_data_with_time']
+                        synthetic_data = synthetic_data.drop(columns=['Measurement ID'], axis=1)  #TODO: verify
+                        csv_bytes = synthetic_data.to_csv(index=False).encode('utf-8')
+                        db = SessionLocal()
+                        new_file = SyntheticDataFile(
+                            filename=f"synthetic_data_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.csv",
+                            content=csv_bytes
+                        )
+                        db.add(new_file)
+                        db.commit()
+                        db.refresh(new_file)
+                        st.success(f"Data saved to database with filename: {new_file.filename}")
+                    except Exception as e:
+                        st.error(f"Error saving data to database: {e}")
 
             if st.button("Evaluate Synthetic Data"):
                 with st.spinner('Evaluating synthetic data...'):
@@ -170,32 +219,20 @@ def generate_data_app():
                     except Exception as e:
                         st.error(f"Error during evaluation: {e}")
 
-            if st.button("Save Synthetic Data to Database"):
-                with st.spinner('Saving synthetic data to database...'):
+            if st.button("Plot Distributions of Real vs. Synthetic Data"):
+                with st.spinner("Plotting..."):
                     try:
-                        generate_downloadable_csv(
-                            st.session_state['generator'].postprocess_timestamps(
-                                st.session_state['synthetic_data_with_time'].copy()
-                            )
-                        )
-                        synthetic_data = st.session_state['synthetic_data_with_time']
-                        csv_bytes = synthetic_data.to_csv(index=False).encode('utf-8')
-                        db = SessionLocal()
-                        new_file = SyntheticDataFile(
-                            filename=f"synthetic_data_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.csv",
-                            content=csv_bytes
-                        )
-                        db.add(new_file)
-                        db.commit()
-                        db.refresh(new_file)
-                        st.success(f"Data saved to database with filename: {new_file.filename}")
+                        generator = st.session_state['generator']
+                        fig = generator.plot_column_distributions(real_data_df, st.session_state['synthetic_data'])
+                        st.set_option('deprecation.showPyplotGlobalUse', False)
+                        st.pyplot(fig)
                     except Exception as e:
-                        st.error(f"Error saving data to database: {e}")
+                        st.error(f"Error during time series comparison: {e}")
 
             selected_column = st.selectbox("Select a column for time series comparison",
                                            selected_columns_for_generation)
             if st.button("Plot Time Series Comparison"):
-                with st.spinner('Plotting time series comparison...'):
+                with st.spinner("Plotting time series comparison..."):
                     try:
                         generator = st.session_state['generator']
                         fig = generator.plot_time_series_comparison(real_data_df, st.session_state['synthetic_data'],
@@ -262,5 +299,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
